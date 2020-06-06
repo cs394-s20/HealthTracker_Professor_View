65a6d356b78e43686fa19c14915bd61e5e513848
quinnshim2021 <quinnshim2021@u.northwestern.edu>
2020-06-06 12:47:04 -0400

    cleaned

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,5 @@
 import firebase from 'firebase/app';
 var firebaseConfig = {
-<<<<<<< HEAD
-    // credentials from firebase should go here
-    // https://firebase.google.com/docs/web/setup
-=======
     apiKey: "your apiKey",
     authDomain: "your authDomain",
     databaseURL: "your databaseURL",
@@ -11,7 +7,7 @@ var firebaseConfig = {
     storageBucket: "your storageBucket",
     messagingSenderId: "your messagingSenderId",
     appId: "your appId"
->>>>>>> db74a6a3e9986ba251937b15db5767c04b28a277
+    // https://firebase.google.com/docs/web/setup
   };
 const Firebase = firebase.initializeApp(firebaseConfig);
 export default Firebase;
